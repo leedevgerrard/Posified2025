a49509e92c451bd7c65d72fee2493427d63a3b7e
Lee Devin Gerrard <leedevgerrard@gmail.com>
2025-11-10 22:34:23 +0700

    chore(backend): initialize project with dependencies and file structure

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+})
