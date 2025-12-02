d8469a7b2385d9af49b6eae350614139bf8a2966
Lee Devin Gerrard <leedevgerrard@gmail.com>
2025-12-02 11:39:06 +0700

    chore: setup query system with tanstack query and axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SnackBarProvider } from 'notistack';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30000
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <SnackBarProvider autoHideDuration={3000}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </SnackBarProvider>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
