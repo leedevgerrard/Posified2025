175ac6807aa83e72634cb37073f5cc4630782b27
Lee Devin Gerrard <leedevgerrard@gmail.com>
2026-01-01 20:01:33 +0700

    feat: add parts of the day adjustment for the greetings

--- frontend/src/components/homePage/Greetings.jsx
+++ frontend/src/components/homePage/Greetings.jsx
@@ -5,9 +5,19 @@ const Greetings = () => {
 
   const userData = useSelector(state => state.user);
 
+  const hour = new Date().getHours();
+  let dayPart = '';
+  if (hour >= 5 && hour < 12) {
+    dayPart = 'Morning';
+  } else if (hour >= 12 && hour < 17) {
+    dayPart = 'Afternoon';
+  } else {
+    dayPart = 'Evening';
+  }
+
   return (
     <div className='px-6 pt-4 pb-2 flex-[1]'>
-      <h1 className='text-3xl font-semibold'>Good Morning, {userData.name || 'Test User'}!</h1>
+      <h1 className='text-3xl font-semibold'>Good {dayPart}, {userData.name || 'Test User'}!</h1>
       <p className='font-semibold'>Break a leg for today!</p>
     </div>
   )
